c90ccc1a7a0ecfd203a57ef51b8aa0871a26da2b
MatiasColazo1 <maty_colazo@hotmail.com>
2023-11-29 13:29:37 -0300

    actualizaciones

--- src/app/components/fotos-form/fotos-form.component.ts
+++ src/app/components/fotos-form/fotos-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget
+}
 
 @Component({
   selector: 'app-fotos-form',
   templateUrl: './fotos-form.component.html',
   styleUrls: ['./fotos-form.component.css']
 })
-export class FotosFormComponent {
+export class FotosFormComponent implements OnInit{
 
+  file: File | null = null;
+  fotoSelected: string | ArrayBuffer | null = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  onFotoSelected(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files[0]) {
+      this.file = inputElement.files[0];
+      // Imagen preview
+      const reader = new FileReader();
+      reader.onload = e => this.fotoSelected = reader.result as string;
+      reader.readAsDataURL(this.file);
+    }
+  }
 }
